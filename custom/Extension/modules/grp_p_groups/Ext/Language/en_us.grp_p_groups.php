037700566c3f10ea3c11bdb79d7639de98f2e1f4
mihir-parikh <mihir1408@gmail.com>
2014-09-20 20:07:40 +1000

    Completion of group address fields

--- custom/Extension/modules/grp_p_groups/Ext/Language/en_us.grp_p_groups.php
+++ custom/Extension/modules/grp_p_groups/Ext/Language/en_us.grp_p_groups.php
@@ -1,3 +1,10 @@
 <?php
 $mod_strings['LBL_NAME'] = 'Group Name';
+$mod_strings['LBL_GROUP_ADDRESS_STREET'] = 'Group street';
+$mod_strings['LBL_GROUP_ADDRESS_STREET_2'] = 'Group Street 2';
+$mod_strings['LBL_GROUP_ADDRESS_STREET_3'] = 'Group Street 3';
+$mod_strings['LBL_GROUP_ADDRESS_CITY'] = 'Group City';
+$mod_strings['LBL_GROUP_ADDRESS_STATE'] = 'Group State';
+$mod_strings['LBL_GROUP_ADDRESS_POSTALCODE'] = 'Group Postalcode';
+$mod_strings['LBL_GROUP_ADDRESS_COUNTRY'] = 'Group Country';
 ?>
--- custom/Extension/modules/grp_p_groups/Ext/Vardefs/vardefs.ext.php
+++ custom/Extension/modules/grp_p_groups/Ext/Vardefs/vardefs.ext.php
@@ -6,50 +6,50 @@ if(!defined("sugarEntry") || !sugarEntry){
 $dictionary["grp_p_groups"]["fields"]["name"]["vname"] = "LBL_NAME";
 
 //Address fields
-$dictionary["grp_p_groups"]["fields"]["primary_address_street"]["name"] = "primary_address_street";
-$dictionary["grp_p_groups"]["fields"]["primary_address_street"]["vname"] = "LBL_PRIMARY_ADDRESS_STREET";
-$dictionary["grp_p_groups"]["fields"]["primary_address_street"]["type"] = "varchar";
-$dictionary["grp_p_groups"]["fields"]["primary_address_street"]["len"] = "150";
-$dictionary["grp_p_groups"]["fields"]["primary_address_street"]["group"] = "primary_address";
-$dictionary["grp_p_groups"]["fields"]["primary_address_street"]["comment"] = "Street address for primary address";
+$dictionary["grp_p_groups"]["fields"]["group_address_street"]["name"] = "group_address_street";
+$dictionary["grp_p_groups"]["fields"]["group_address_street"]["vname"] = "LBL_GROUP_ADDRESS_STREET";
+$dictionary["grp_p_groups"]["fields"]["group_address_street"]["type"] = "varchar";
+$dictionary["grp_p_groups"]["fields"]["group_address_street"]["len"] = "150";
+$dictionary["grp_p_groups"]["fields"]["group_address_street"]["group"] = "group_address";
+$dictionary["grp_p_groups"]["fields"]["group_address_street"]["comment"] = "Street address for group address";
 
-$dictionary["grp_p_groups"]["fields"]["primary_address_street_2"]["name"] = "primary_address_street_2";
-$dictionary["grp_p_groups"]["fields"]["primary_address_street_2"]["vname"] = "LBL_PRIMARY_ADDRESS_STREET_2";
-$dictionary["grp_p_groups"]["fields"]["primary_address_street_2"]["type"] = "varchar";
-$dictionary["grp_p_groups"]["fields"]["primary_address_street_2"]["len"] = "150";
-$dictionary["grp_p_groups"]["fields"]["primary_address_street_2"]["source"] = "non-db";
+$dictionary["grp_p_groups"]["fields"]["group_address_street_2"]["name"] = "group_address_street_2";
+$dictionary["grp_p_groups"]["fields"]["group_address_street_2"]["vname"] = "LBL_GROUP_ADDRESS_STREET_2";
+$dictionary["grp_p_groups"]["fields"]["group_address_street_2"]["type"] = "varchar";
+$dictionary["grp_p_groups"]["fields"]["group_address_street_2"]["len"] = "150";
+$dictionary["grp_p_groups"]["fields"]["group_address_street_2"]["source"] = "non-db";
 
-$dictionary["grp_p_groups"]["fields"]["primary_address_street_3"]["name"] = "primary_address_street_3";
-$dictionary["grp_p_groups"]["fields"]["primary_address_street_3"]["vname"] = "LBL_PRIMARY_ADDRESS_STREET_3";
-$dictionary["grp_p_groups"]["fields"]["primary_address_street_3"]["type"] = "varchar";
-$dictionary["grp_p_groups"]["fields"]["primary_address_street_3"]["len"] = "150";
-$dictionary["grp_p_groups"]["fields"]["primary_address_street_3"]["source"] = "non-db";
+$dictionary["grp_p_groups"]["fields"]["group_address_street_3"]["name"] = "group_address_street_3";
+$dictionary["grp_p_groups"]["fields"]["group_address_street_3"]["vname"] = "LBL_GROUP_ADDRESS_STREET_3";
+$dictionary["grp_p_groups"]["fields"]["group_address_street_3"]["type"] = "varchar";
+$dictionary["grp_p_groups"]["fields"]["group_address_street_3"]["len"] = "150";
+$dictionary["grp_p_groups"]["fields"]["group_address_street_3"]["source"] = "non-db";
 
-$dictionary["grp_p_groups"]["fields"]["primary_address_city"]["name"] = "primary_address_city";
-$dictionary["grp_p_groups"]["fields"]["primary_address_city"]["vname"] = "LBL_PRIMARY_ADDRESS_CITY";
-$dictionary["grp_p_groups"]["fields"]["primary_address_city"]["type"] = "varchar";
-$dictionary["grp_p_groups"]["fields"]["primary_address_city"]["len"] = "100";
-$dictionary["grp_p_groups"]["fields"]["primary_address_city"]["group"] = "primary_address";
-$dictionary["grp_p_groups"]["fields"]["primary_address_city"]["comment"] = "City for primary address";
+$dictionary["grp_p_groups"]["fields"]["group_address_city"]["name"] = "group_address_city";
+$dictionary["grp_p_groups"]["fields"]["group_address_city"]["vname"] = "LBL_GROUP_ADDRESS_CITY";
+$dictionary["grp_p_groups"]["fields"]["group_address_city"]["type"] = "varchar";
+$dictionary["grp_p_groups"]["fields"]["group_address_city"]["len"] = "100";
+$dictionary["grp_p_groups"]["fields"]["group_address_city"]["group"] = "group_address";
+$dictionary["grp_p_groups"]["fields"]["group_address_city"]["comment"] = "City for group address";
 
-$dictionary["grp_p_groups"]["fields"]["primary_address_state"]["name"] = "primary_address_state";
-$dictionary["grp_p_groups"]["fields"]["primary_address_state"]["vname"] = "LBL_PRIMARY_ADDRESS_STATE";
-$dictionary["grp_p_groups"]["fields"]["primary_address_state"]["type"] = "varchar";
-$dictionary["grp_p_groups"]["fields"]["primary_address_state"]["len"] = "100";
-$dictionary["grp_p_groups"]["fields"]["primary_address_state"]["group"] = "primary_address";
-$dictionary["grp_p_groups"]["fields"]["primary_address_state"]["comment"] = "State for primary address";
+$dictionary["grp_p_groups"]["fields"]["group_address_state"]["name"] = "group_address_state";
+$dictionary["grp_p_groups"]["fields"]["group_address_state"]["vname"] = "LBL_GROUP_ADDRESS_STATE";
+$dictionary["grp_p_groups"]["fields"]["group_address_state"]["type"] = "varchar";
+$dictionary["grp_p_groups"]["fields"]["group_address_state"]["len"] = "100";
+$dictionary["grp_p_groups"]["fields"]["group_address_state"]["group"] = "group_address";
+$dictionary["grp_p_groups"]["fields"]["group_address_state"]["comment"] = "State for group address";
 
-$dictionary["grp_p_groups"]["fields"]["primary_address_postalcode"]["name"] = "primary_address_postalcode";
-$dictionary["grp_p_groups"]["fields"]["primary_address_postalcode"]["vname"] = "LBL_PRIMARY_ADDRESS_POSTALCODE";
-$dictionary["grp_p_groups"]["fields"]["primary_address_postalcode"]["type"] = "varchar";
-$dictionary["grp_p_groups"]["fields"]["primary_address_postalcode"]["len"] = "20";
-$dictionary["grp_p_groups"]["fields"]["primary_address_postalcode"]["group"] = "primary_address";
-$dictionary["grp_p_groups"]["fields"]["primary_address_postalcode"]["comment"] = "Postal code for primary address";
+$dictionary["grp_p_groups"]["fields"]["group_address_postalcode"]["name"] = "group_address_postalcode";
+$dictionary["grp_p_groups"]["fields"]["group_address_postalcode"]["vname"] = "LBL_GROUP_ADDRESS_POSTALCODE";
+$dictionary["grp_p_groups"]["fields"]["group_address_postalcode"]["type"] = "varchar";
+$dictionary["grp_p_groups"]["fields"]["group_address_postalcode"]["len"] = "20";
+$dictionary["grp_p_groups"]["fields"]["group_address_postalcode"]["group"] = "group_address";
+$dictionary["grp_p_groups"]["fields"]["group_address_postalcode"]["comment"] = "Postal code for group address";
 
-$dictionary["grp_p_groups"]["fields"]["primary_address_country"]["name"] = "primary_address_country";
-$dictionary["grp_p_groups"]["fields"]["primary_address_country"]["vname"] = "LBL_PRIMARY_ADDRESS_COUNTRY";
-$dictionary["grp_p_groups"]["fields"]["primary_address_country"]["type"] = "varchar";
-$dictionary["grp_p_groups"]["fields"]["primary_address_country"]["group"] = "primary_address";
-$dictionary["grp_p_groups"]["fields"]["primary_address_country"]["comment"] = "Country for primary address";
+$dictionary["grp_p_groups"]["fields"]["group_address_country"]["name"] = "group_address_country";
+$dictionary["grp_p_groups"]["fields"]["group_address_country"]["vname"] = "LBL_GROUP_ADDRESS_COUNTRY";
+$dictionary["grp_p_groups"]["fields"]["group_address_country"]["type"] = "varchar";
+$dictionary["grp_p_groups"]["fields"]["group_address_country"]["group"] = "group_address";
+$dictionary["grp_p_groups"]["fields"]["group_address_country"]["comment"] = "Country for group address";
 ?>
 
