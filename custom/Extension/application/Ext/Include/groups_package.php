25ce85865dd5010a99a0a552e5b724b9cfadc2a1
mihir-parikh <mihir1408@gmail.com>
2014-09-20 14:13:37 +1000

    Initial setup of Groups module

---
+++ custom/Extension/application/Ext/Include/groups_package.php
@@ -0,0 +1,7 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['grp_p_groups'] = 'grp_p_groups';
+$beanFiles['grp_p_groups'] = 'modules/grp_p_groups/grp_p_groups.php';
+$moduleList[] = 'grp_p_groups';
+
+?>
