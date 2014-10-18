6a7cfe428e2d6494530a6500b4aa52396845c126
mihir-parikh <mihir1408@gmail.com>
2014-10-18 13:12:58 +1100

    Automatically assigning a user in cases

---
+++ custom/modules/Cases/views/view.edit.php
@@ -0,0 +1,9 @@
+<?php
+
+class CustomCasesViewEdit extends ViewEdit{
+    function display() {
+        $this->bean->assigned_user_id = "4abda5b9-b267-1f2d-460c-5425691c46d2";
+        parent::display();
+    }
+}
+
