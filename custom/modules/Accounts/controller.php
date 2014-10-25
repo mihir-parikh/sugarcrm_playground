f1d081deefd58373e753062e9e4cc810356bee37
mihir-parikh <mihir1408@gmail.com>
2014-10-25 22:58:00 +1100

    A jQuery ajax call to retrieve quote info

--- custom/modules/Accounts/controller.php
+++ custom/modules/Accounts/controller.php
@@ -11,4 +11,31 @@ class CustomAccountsController extends SugarController {
         $GLOBALS['view'] = $this->view;
         $this->bean->billing_address_city = strtoupper($this->bean->billing_address_city);
     }
+    
+    public function action_retrieve_quote(){
+        $account_bean = BeanFactory::getBean("Accounts");
+        $account_bean->retrieve_by_string_fields(array("name" => $_REQUEST["name"]));
+                
+        $quote_array = array();
+        
+        if(empty($account_bean->id)){
+            $quote_array["status"] = 0;
+        }
+        else{
+            //Retrieve Annual Revenue information for the current bean
+            $annual_revenue = $account_bean->annual_revenue;
+            
+            if(!empty($annual_revenue)){
+                $quote_array["status"] = 1;                
+                $quote_array["quote"] = $annual_revenue;
+            }
+            else{
+                $quote_array["status"] = 0;            
+            }
+        }        
+        
+        //Print the array to utilise in javascript
+        echo json_encode($quote_array);        
+        exit();
+    }
 }
--- custom/modules/Accounts/custom_accounts.js
+++ custom/modules/Accounts/custom_accounts.js
@@ -2,4 +2,28 @@ $(document).ready(function(){
     $("#phone_office").click(function(){
         alert("The correct phone number format is: +xx-xxxxxxxxx");
     });
+    
+    $("#ticker_symbol").focusout(function(){
+        //A bean must already be existing to fetch revenue information of it. I.e.
+        //This is implemented to function only while a bean is being updated.
+        var account_name = $("#name").val();
+        
+        if($.trim(account_name).length > 0){                    
+            if($.trim($("#ticker_symbol").val()).length > 0){
+                $.ajax({
+                    url: "index.php?module=Accounts&action=retrieve_quote&name="+account_name
+                }).done(function(data){
+                    var quoteObj = jQuery.parseJSON(data);                    
+                    if(quoteObj.status === 1){
+                        if(quoteObj.quote.length > 2){
+                            $("#quote_c").val(quoteObj.quote.substr(0, 2));
+                        }
+                        else{
+                            $("#quote_c").val(quoteObj.quote);
+                        }                        
+                    }
+                });            
+            }
+        }
+    });
 });
