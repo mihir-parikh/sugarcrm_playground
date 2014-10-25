4b41c36299307505065e0d24896824b58e3781fe
mihir-parikh <mihir1408@gmail.com>
2014-10-25 19:38:03 +1100

    Creation of followup call

--- custom/modules/Contacts/controller.php
+++ custom/modules/Contacts/controller.php
@@ -23,6 +23,20 @@ class CustomContactsController extends SugarController {
     }
     
     public function action_schedule_followup(){
+        //Create a new Calls bean
+        $calls_bean = BeanFactory::getBean("Calls");
+        $calls_bean->name = "A followup call";
+        $calls_bean->direction = "Outbound";
+        $calls_bean->status = "Planned";
         
+        $unix_timestamp = strtotime("+7 day");
+        $datetime = date("Y-m-d H:i:s", $unix_timestamp);
+        $calls_bean->date_start = $datetime;
+        
+        $calls_bean->duration_hours = 0;
+        $calls_bean->duration_minutes = 30;
+        $calls_bean->save();
+        
+        SugarApplication::redirect("index.php?module=Contacts&action=DetailView&record=".$this->bean->id);
     }
 }
