0fb5e2aaca5fc7952bb341095739013a0b34baa7
mihir-parikh <mihir1408@gmail.com>
2014-10-25 20:48:23 +1100

    Added Quote field in detail and edit views

---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_quote_c.php
@@ -0,0 +1,5 @@
+<?php
+ // created: 2014-10-25 11:41:30
+$dictionary['Account']['fields']['quote_c']['labelValue']='Quote';
+
+ ?>
---
+++ custom/modules/Accounts/language/en_us.lang.php
@@ -0,0 +1,5 @@
+<?php
+// created: 2014-10-25 11:41:30
+$mod_strings = array (
+  'LBL_QUOTE' => 'Quote',
+);
